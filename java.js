cc3e7dc1763f44e44422e23d60967d80e9844d06
NaufalFADH27 <batubaranaufal8@gmail.com>
2026-05-11 21:46:14 +0700

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,4 @@
-<script>
-    
-    // ---- Navbar scroll ----
+ // ---- Navbar scroll ----
     const navbar = document.getElementById('navbar');
     window.addEventListener('scroll', () => {
       navbar.classList.toggle('scrolled', window.scrollY > 60);
